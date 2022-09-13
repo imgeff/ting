5a5238e06d19b712230ce7e2d27cd4ded78e0485
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-09-13 18:20:02 -0300

    :sparkles: feat: Implementa função process
    responsável por gerenciar o processamento de leitura dos arquivos

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+files_processed = {}
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    try:
+        files_processed[path_file]
+    except KeyError:
+        file = txt_importer(path_file)
+        file_processed = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+        instance.enqueue(file_processed)
+        print(file_processed, file=sys.stdout)
+        files_processed[path_file] = file_processed
 
 
 def remove(instance):
