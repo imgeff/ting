f9e314a56923efb8fd518a570599a3095c532676
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-09-13 16:59:21 -0300

    :sparkles: feat: Implementa função txt_importer
    responsável por ler arquivos e retornar uma lista de linhas desse arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import csv
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if (path_file.endswith(".txt")):
+            lines = []
+            with open(path_file, ) as file:
+                file_reader = csv.reader(file, delimiter="\n")
+                for line in file_reader:
+                    lines.append(line[0])
+                return lines
+        print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
