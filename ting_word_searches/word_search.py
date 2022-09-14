668b7b500c8cf87841d01d6929fdc6852e3194e8
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-09-14 19:27:08 -0300

    :sparkles: feat: Implementa função exists_word
    responsável por montar um relatório da ocorrência da palavra pesquisada nos arquivos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue = instance.getQueue()
+    result = []
+    for file in queue:
+        report_file = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for index, linha in enumerate(file["linhas_do_arquivo"]):
+            if(word.lower() in linha.lower()):
+                report_file["ocorrencias"].append({"linha": index + 1})
+        if(len(report_file["ocorrencias"])):
+            result.append(report_file)
+    return result
 
 
 def search_by_word(word, instance):
